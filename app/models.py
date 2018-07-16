9b66a3b8bafcab8e5a4c5c6545dbc181681c5182
james-chege <jymeschege@gmail.com>
2018-07-16 15:27:35 +0300

    [CHORE] add Model for add new entries

--- app/models.py
+++ app/models.py
@@ -2,3 +2,27 @@
 
 # contains models for the app
 
+import datetime
+
+from flask_restful import Resource
+
+class Entry(Resource):
+    """Add new entry"""
+
+
+    # constructor
+    def __init__(self):
+        # all entries placeholders
+        self.entries = []
+
+    def add_entry(self, title, description):
+        """Adds new entries"""
+
+        now = datetime.datetime.now()
+        date_created = now.strftime("%Y-%m-%d %H:%M")
+        single_entry_holder = dict()
+        single_entry_holder['title'] = title
+        single_entry_holder['description'] = description
+        single_entry_holder['created'] = str(date_created)
+        self.entries.append(single_entry_holder)
+        

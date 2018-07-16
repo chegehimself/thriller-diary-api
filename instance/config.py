3dce0e410bdd5998bdf3cc34a3e18966f206586e
james-chege <jymeschege@gmail.com>
2018-07-16 15:07:38 +0300

    [CHORE] Add environment config

--- instance/config.py
+++ instance/config.py
@@ -2,3 +2,27 @@
 
 # For environment configuration
 
+# config.py
+
+# contains environment settings
+
+import os
+
+class Config(object):
+    """Sharing variables configs (cross origin etc)"""
+    DEBUG = False
+    CSRF_ENABLED = True
+
+class DevelopmentConfig(Config):
+    """Development environment variables config."""
+    DEBUG = True
+
+class ProductionConfig(Config):
+    """Production environment variables config."""
+    DEBUG = False
+    TESTING = False
+
+app_config = {
+    'development': DevelopmentConfig,
+    'production': ProductionConfig,
+}

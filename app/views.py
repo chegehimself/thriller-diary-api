086e1d7fee9c3b6616fad2d6765759db266ba372
james-chege <jymeschege@gmail.com>
2018-07-16 17:31:29 +0300

    FEATURE #159044096 - implement post

--- app/views.py
+++ app/views.py
@@ -4,10 +4,15 @@
 
 from flask import Flask, Blueprint, jsonify, request, make_response
 
+# import models
+from app.models import Entry
+ENTRY = Entry()
+
 # create entries and a single entry Blueprint and
 # version the urls to have '/api/v1' prefix
 entries_bp = Blueprint('entries',__name__, url_prefix='/api/v1')
 
+# deal with single entry
 ent_bp = Blueprint('ent', __name__, url_prefix='/api/v1')
 
 @entries_bp.route('/', methods=['POST', 'GET'])
@@ -27,3 +32,14 @@ def entries():
     """Retrives all Entries"""
     if request.method == 'GET':
         return "OK! entries"
+
+
+@ent_bp.route('/entries', methods=['POST'])
+def add_new_entry():
+    """Add an entry"""
+    json_data = request.get_json()
+    title = json_data['title']
+    description = json_data['description']
+    ENTRY.add_entry(title,description)
+    response = {"status": "success", "entry": json_data}, 201
+    return response

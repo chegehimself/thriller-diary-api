9f5831962d57404e39b48590c322fb30abef91f0
james-chege <jymeschege@gmail.com>
2018-07-18 05:03:55 +0300

    FEATURE - #159044236 Display message for errors on deletion

--- app/views.py
+++ app/views.py
@@ -93,6 +93,8 @@ def delete_entry(id):
     if not entries:
         return {"status": "Fail", "entry": {"Error":"That entry does not exist!"}}, 404
     for i, entry in  enumerate(entries):
+        if entry['id'] != id:
+            return {"status": "Fail", "entry": {"Error":"That entry does not exist!"}}, 404
         # check if the entry exists
         if entry['id'] == int(id):
             title = entry['title']
